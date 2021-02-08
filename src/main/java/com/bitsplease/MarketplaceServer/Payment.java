1b041ba4d58893f95065a0694c2eb39a20d7f403
Hugo Bollon <hugo.bollon@gmail.com>
2021-02-08 13:28:30 +0100

    Get mangopay client id if exist on payment

--- src/main/java/com/bitsplease/MarketplaceServer/Payment.java
+++ src/main/java/com/bitsplease/MarketplaceServer/Payment.java
@@ -2,14 +2,16 @@ package com.bitsplease.MarketplaceServer;
 
 import com.bitsplease.MarketplaceServer.model.Client;
 import com.bitsplease.MarketplaceServer.model.Product;
+import com.bitsplease.MarketplaceServer.model.requestObjects.BuyRequest;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import javax.net.ssl.*;
 import javax.ws.rs.*;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.URL;
-import java.security.cert.X509Certificate;
 import java.util.Base64;
 
 @Path("/mangopay")
@@ -24,13 +26,13 @@ public class Payment {
     @Consumes("application/json")
     @Produces("application/json")
     public String pay(
-            Product product
+            BuyRequest obj
     ) {
         try {
-            int userId = (int)(Math.random()*10000)+1;
+            int userId = getClientId(obj.getClient().getEmail());
             String webCardRequestJson = new JSONObject()
-                    .put("AuthorId", 100218276)
-                    .put("DebitedFunds", new JSONObject().put("Currency", "EUR").put("Amount", (product.getPrice() + (product.getWeight() * 0.3)) * 100))
+                    .put("AuthorId", userId)
+                    .put("DebitedFunds", new JSONObject().put("Currency", "EUR").put("Amount", (obj.getProduct().getPrice() + (obj.getProduct().getWeight() * 0.3)) * 100))
                     .put("Fees", new JSONObject().put("Currency", "EUR").put("Amount", "0"))
                     .put("ReturnURL", "http://localhost:4200")
                     .put("CardType", "CB_VISA_MASTERCARD")
@@ -38,7 +40,7 @@ public class Payment {
                     .put("Culture", "FR")
                     .toString();
 
-            URL webCardUrl = new URL(MANGOPAY_URL+"/payins/card/web");
+            URL webCardUrl = new URL(MANGOPAY_URL + "/payins/card/web");
             HttpsURLConnection con = (HttpsURLConnection)webCardUrl.openConnection();
             con.setRequestMethod("POST");
             con.setRequestProperty("Authorization", AUTHORIZATION_HEADER);
@@ -60,11 +62,49 @@ public class Payment {
 
             System.out.println("Resp Code:" + con.getResponseCode());
             System.out.println("Resp Message:" + con.getResponseMessage());
-
             return response;
         } catch (Exception e) {
             e.printStackTrace();
             return e.toString();
         }
     }
+
+    public int getClientId(String email) throws IOException {
+        URL url = new URL(MANGOPAY_URL + "/users/");
+        System.out.println(url.toString());
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("Authorization", AUTHORIZATION_HEADER);
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+        int responseCode = con.getResponseCode();
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            System.out.println(response.toString());
+            try {
+                JSONArray responseJson = new JSONArray(response.toString());
+                for (int i = 0; i < responseJson.length(); i++) {
+                    JSONObject obj = responseJson.getJSONObject(i);
+                    if(obj.getString("Email").equals(email)) {
+                        return obj.getInt("Id");
+                    }
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return -1;
+        } else {
+            System.out.println("GET request not worked");
+        }
+        return -1;
+    }
+
 }
