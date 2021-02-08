78b6698a5e41000ca784eafed16f27a3ad49f26d
Hugo Bollon <hugo.bollon@gmail.com>
2021-02-08 09:37:54 +0100

    Add BuyRequest and Client models

--- src/main/java/com/bitsplease/MarketplaceServer/model/Client.java
+++ src/main/java/com/bitsplease/MarketplaceServer/model/Client.java
@@ -1,4 +1,61 @@
 package com.bitsplease.MarketplaceServer.model;
 
 public class Client {
+    private String firstName;
+    private String lastName;
+    private String birthday;
+    private String nationality;
+    private String countryOfResidence;
+    private String email;
+
+    public Client() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getCountryOfResidence() {
+        return countryOfResidence;
+    }
+
+    public void setCountryOfResidence(String countryOfResidence) {
+        this.countryOfResidence = countryOfResidence;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
---
+++ src/main/java/com/bitsplease/MarketplaceServer/model/requestObjects/BuyRequest.java
@@ -0,0 +1,28 @@
+package com.bitsplease.MarketplaceServer.model.requestObjects;
+
+import com.bitsplease.MarketplaceServer.model.Client;
+import com.bitsplease.MarketplaceServer.model.Product;
+
+public class BuyRequest {
+    private Client client;
+    private Product product;
+
+    public BuyRequest() {
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
