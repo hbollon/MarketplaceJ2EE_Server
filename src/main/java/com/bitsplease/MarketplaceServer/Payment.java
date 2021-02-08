019390200359022032e62f2095ce799e7c81d546
Hugo Bollon <hugo.bollon@gmail.com>
2021-02-08 09:31:12 +0100

    Fix https mangopay sandbox cert issue + Response return

--- src/main/java/com/bitsplease/MarketplaceServer/Payment.java
+++ src/main/java/com/bitsplease/MarketplaceServer/Payment.java
@@ -1,15 +1,14 @@
 package com.bitsplease.MarketplaceServer;
 
 import com.bitsplease.MarketplaceServer.model.Product;
-import org.json.JSONException;
 import org.json.JSONObject;
 
-import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.*;
 import javax.ws.rs.*;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.IOException;
 import java.net.URL;
+import java.security.cert.X509Certificate;
 import java.util.Base64;
 
 @Path("/mangopay")
@@ -27,15 +26,16 @@ public class Payment {
     ) {
         try {
             final String httpsURL = "https://api.sandbox.mangopay.com/v2.01/info802marketplace";
+            this.FixHttpsCert();
 
             int userId = (int)(Math.random()*10000)+1;
             String webCardRequestJson = new JSONObject()
-                    .put("AuthorId", userId)
+                    .put("AuthorId", 100218276)
                     .put("DebitedFunds", new JSONObject().put("Currency", "EUR").put("Amount", (product.getPrice() + (product.getWeight() * 0.3)) * 100))
                     .put("Fees", new JSONObject().put("Currency", "EUR").put("Amount", "0"))
                     .put("ReturnURL", "http://localhost:4200")
                     .put("CardType", "CB_VISA_MASTERCARD")
-                    .put("CreditedWalletId", "20000")
+                    .put("CreditedWalletId", "100218510")
                     .put("Culture", "FR")
                     .toString();
 
@@ -54,17 +54,51 @@ public class Payment {
             output.close();
             DataInputStream input = new DataInputStream( con.getInputStream() );
 
+            String response = "";
             for( int c = input.read(); c != -1; c = input.read() )
-                System.out.print( (char)c );
+                response = response.concat(String.valueOf((char)c));
             input.close();
 
-            System.out.println("Resp Code:"+con .getResponseCode());
-            System.out.println("Resp Message:"+ con .getResponseMessage());
+            System.out.println("Resp Code:" + con.getResponseCode());
+            System.out.println("Resp Message:" + con.getResponseMessage());
 
-            return con.getResponseMessage();
+            return response;
         } catch (Exception e) {
             e.printStackTrace();
             return e.toString();
         }
+    }
+
+    /**
+     * Used to get rid of: "javax.net.ssl.SSLHandshakeException: PKIX path building failed:
+     * sun.security.provider.certpath.SunCertPathBuilderException:
+     * unable to find valid certification path to requested target" with mangopay sandbox api
+     *
+     * It override the SSL checking process
+     * @throws Exception
+     */
+    public void FixHttpsCert() throws Exception {
+        TrustManager[] trustAllCerts = new TrustManager[] {
+                new X509TrustManager() {
+                    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                        return null;
+                    }
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) {  }
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) {  }
+                }
+        };
+
+        SSLContext sc = SSLContext.getInstance("SSL");
+        sc.init(null, trustAllCerts, new java.security.SecureRandom());
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+        // Create all-trusting host name verifier
+        HostnameVerifier allHostsValid = new HostnameVerifier() {
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        };
+        // Install the all-trusting host verifier
+        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
     }
 }
