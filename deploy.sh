882c2ef0b71f5ad6abafbd2f42495c8d843ca96b
Hugo Bollon <hugo.bollon@gmail.com>
2021-03-18 16:46:03 +0100

    Save go server pid to file on start and ask for redeployment on deploy

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,25 @@
 # Stop Glassfish server
 ~/Softwares/glassfish/glassfish5/glassfish/bin/asadmin stop-domain domain2
 
-# Update repo
+# Check if the Go server is already running and ask for killing if yes
 cd ~/Softwares/MarketplaceJ2EE_Server
+PID_FILE="graphql/pid.txt"
+RUNNING = false
+if test -f "$PID_FILE"; then
+    echo $PID_FILE
+    if kill -0 $(head -n 1 $PID_FILE) > /dev/null 2>&1; then
+        read -p "The Go server is already running. Do you want to kill it and continue? (y/n)" -n 1 -r
+        echo
+        if [[ $REPLY =~ ^[Yy]$ ]]
+        then
+            kill $(head -n 1 $PID_FILE)
+        else
+            RUNNING = true
+        fi
+    fi
+fi
+
+# Update repo
 git reset --hard
 git pull
 
@@ -18,8 +35,12 @@ mvn clean install
 echo "J2EE server launched !"
 
 # Launch graphql go server
-cd graphql
-mkdir ../outputs
-touch ../outputs/graphql_server.log
-go run ./ &>> ../outputs/graphql_server.log &
-echo "Graphql go server launched !"
+if !RUNNING then 
+    cd graphql
+    mkdir ../outputs
+    touch ../outputs/graphql_server.log
+    go run ./ &>> ../outputs/graphql_server.log & echo $! > graphql/pid.txt
+    echo "Graphql go server launched !"
+else 
+    echo "Graphql go server starting skipped !"
+fi
